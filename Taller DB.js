26865568422246cdee2dd3ad93f991251629f6ef
akdevlabs <116324447+akdevlabs@users.noreply.github.com>
2024-07-09 22:46:33 -0600

    Add files via upload

---
+++ Taller DB.js
@@ -0,0 +1,54 @@
+export const profile01 = {
+ 
+
+
+
+
+
+}
+
+export const profile02 = {
+  name:' TALLER 2',  
+  img:'  Images/pics/girl.webp',
+  stars: 10,
+  payment:["cash","cc"],
+  horario:[
+              "M-F",
+              "9:00",
+              "20:00",
+              "s",
+              "9:00",
+              "17:00"
+            ],
+  availability:[
+                    "Monday",
+                    "Tuesday",
+                    "Wensday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday"
+                ],
+  Id:202301,
+  type:"Mecanico",
+  vet:1,
+  backImg:"Images/pics/tbd.webp", 
+  album:[
+            "Images/pics/repair toools.webp",
+            "Images/pics/repairing.webp",
+            "Images/pics/hardwork.webp",
+            "Images/pics/breaks.webp",
+            "Images/pics/oil.webp",
+            "Images/pics/carhood.webp"
+          ],
+
+
+  website:"www.buscastaller.com",
+  phone:"(XXX)XXX-XXXX",
+  adress:"Taller Mecánico oldcar, 72353 Puebla, Pue.",
+  googleAd:"https://www.google.com/maps/place/Bodega+Aurrera,+11+Sur/@19.0367331,-98.2110603,18z/data=!4m6!3m5!1s0x85cfc0cf259c04bf:0xaf3c1688f674bf3e!8m2!3d19.035654!4d-98.2124931!16s%2Fg%2F1tdq6bwv?entry=ttu",
+  email:"Carfix@gmail.com",
+  cc:["visa","master","amex"],
+  promo:"https://docs.google.com/spreadsheets/d/e/2PACX-1vTngEGvyGN5dZFxvW1UE6Jmi34AIhl3XtHEslOtifkkJAHL3AQnHS_oQn8IlYEjM6yYjeBy5MzlJZqL/pubhtml?gid=0&single=true",
+  pList:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQe_eiU1hcs5SqB3YA2Lhzy4Gd7ZxnfGY1Vy7t11DQAXcUeUfqHnQWcGnfl2QXolYYQuVUFngefapse/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
+}
